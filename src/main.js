c2f651d305cfcfc0c4767e9cfdbf4ef734adf7ec
Nicole Perkins <perkinsn@ufl.edu>
2021-03-21 15:56:43 -0500

    add timeout function

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
 var boilingTemp;
-boilingTemp = () => "212";
+boilingTemp = () => 212;
 document.getElementById('boiling-temp').innerHTML = boilingTemp();
 
 var freezingTemp;
-freezingTemp = () => "32";
+freezingTemp = () => 32;
 document.getElementById('freezing-temp').innerHTML = freezingTemp();
 
 var waterTemp;
-waterTemp = () => "63";
+waterTemp = () => 63;
 document.getElementById('water-temp').innerHTML = waterTemp();
+
+var boiling = document.getElementById("boling");
+var frozen = document.getElementById("frozen");
+var goodTemp = document.getElementById("good-temp");
+
+setTimeout(function show() {
+    if (waterTemp > 212) {
+      boiling.classList.remove("hide");
+      boiling.classList.add("show");
+    }
+  
+    else if (waterTemp < 32) {
+      frozen.classList.remove("hide");
+      frozen.classList.add("show");
+    }
+  
+    else {
+      goodTemp.classList.remove("hide");
+      goodTemp.classList.add("show");
+    }
+  }, 3000);
+
+
